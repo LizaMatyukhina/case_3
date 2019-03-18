6d6678db8aa803f79df110521a09781bc60373b8
LizaMatyukhina <liza.matyukhina2000@gmail.com>
2019-03-18 19:27:49 +0700

    drakon commit

--- drakon.py
+++ drakon.py
@@ -8,19 +8,19 @@ def drakon(order, x1, x2, y1, y2):
         turtle.down()
         turtle.goto(x2, y2)
     else:
-        dx= (x2 - x1)/2
-        dy= (y2 - y1)/2
+        dx = (x2 - x1)/2
+        dy = (y2 - y1)/2
         nx = x1 + dx - dy
         ny = y1 + dy + dx
-        drakon(order-1, x2, nx, y2, ny)
-        drakon(order-1, x1, nx, y1, ny)
+        drakon(order - 1, x2, nx, y2, ny)
+        drakon(order - 1, x1, nx, y1, ny)
 
 
 def main():
     turtle.speed(0)
     n = int(input(GLUB_OF_SIDE))
     a = int(input(LENGHT))
-    x1 = -a
+    x1 = - a
     x2 = a
     y1 = 0
     y2 = 0
